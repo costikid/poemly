8ada5cce862cc193be3b05cf4db3568e4c2f02ec
costikid <costanza.casullo@hotmail.it>
2024-04-22 21:22:45 +0200

    removed unused file

--- frontend/src/apiConfig.js
+++
@@ -1,8 +0,0 @@
-
-export const API_BASE_URL = 'http://localhost:3000/api';
-export const UPDATE_DETAILS_URL = `${API_BASE_URL}/update-details`;
-export const CHANGE_PASSWORD_URL = `${API_BASE_URL}/change-password`;
-export const DELETE_ACCOUNT_URL = `${API_BASE_URL}/delete`;
-export const USER_POEMS_URL = `${API_BASE_URL}/poems/user`;
-export const SINGLE_POEM_URL = `${API_BASE_URL}/poems`;
-
