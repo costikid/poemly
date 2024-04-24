d9bc1e5034a02fee00410d72c5e714d164bc08a7
costikid <costanza.casullo@hotmail.it>
2024-04-24 16:53:11 +0200

    new comment on file I am not using

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -6,3 +6,4 @@ export const DELETE_ACCOUNT_URL = `${API_BASE_URL}/delete`;
 export const USER_POEMS_URL = `${API_BASE_URL}/poems/user`;
 export const SINGLE_POEM_URL = `${API_BASE_URL}/poems`;
 
+//not using this file. 
